e339a1dd2489005ca4b81f5bf2962271e7c2b806
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2014-09-24 18:21:31 +0200

    Refactoring some methods.

--- src/main/scala/org/learningconcurrency/ch7/TransactionalCollections.scala
+++ src/main/scala/org/learningconcurrency/ch7/TransactionalCollections.scala
@@ -22,18 +22,12 @@ object TransactionLocals extends App {
   }
 
   val myList = new TSortedList().insert(14).insert(22)
-  val f = Future {
-    atomic { implicit txn =>
-      clearList(myList) 
-      myLog()
-    }
+  def clearWithLog(): String = atomic { implicit txn =>
+    clearList(myList) 
+    myLog()
   }
-  val g = Future {
-    atomic { implicit txn =>
-      clearList(myList)
-      myLog()
-    }
-  }
+  val f = Future { clearWithLog() }
+  val g = Future { clearWithLog() }
   for (h1 <- f; h2 <- g) {
     log(s"Log for f: $h1\nLog for g: $h2")
   }
@@ -47,15 +41,15 @@ object TransactionalArray extends App {
   import scala.concurrent.stm._
 
   val pages = Seq.fill(5)("Scala 2.10 is out, " * 7)
-  val doc = TArray(pages)
+  val website = TArray(pages)
 
   def replace(pat: String, txt: String): Unit = atomic { implicit txn =>
-    for (i <- 0 until doc.length) doc(i) = doc(i).replace(pat, txt)
+    for (i <- 0 until website.length) website(i) = website(i).replace(pat, txt)
   }
 
   def asString = atomic { implicit txn =>
     var s: String = ""
-    for (i <- 0 until doc.length) s += s"Page $i\n=======\n${doc(i)}\n\n"
+    for (i <- 0 until website.length) s += s"Page $i\n=======\n${website(i)}\n\n"
     s
   }
 
