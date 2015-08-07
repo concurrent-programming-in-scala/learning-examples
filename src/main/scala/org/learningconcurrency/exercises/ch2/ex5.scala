d8b4ce9e4614a8757c1b3d450d303eaa482a58fa
Alexander Ryblov <ryblov.a.v@gmail.com>
2015-08-07 09:26:17 +0300

    add answer to exercise 5 chapter 2

---
+++ src/main/scala/org/learningconcurrency/exercises/ch2/ex5.scala
@@ -0,0 +1,61 @@
+package org.learningconcurrency
+package exercises
+package ch2
+
+object Ex5 extends App {
+
+  class SyncVar[T] {
+
+    private var empty: Boolean = true
+
+    private var x: T = null.asInstanceOf[T]
+
+    def isEmpty = empty
+
+    def nonEmpty = !empty
+
+    def getWait():T = this.synchronized {
+      while (empty)
+        this.wait
+
+      empty = true
+      this.notify
+      x
+    }
+
+    def putWait(x: T): Unit = this.synchronized {
+      while (!empty)
+        this.wait()
+
+      empty = false
+      this.x = x
+      this.notify()
+    }
+
+
+  }
+
+  import org.learningconcurrency.ch2.thread
+
+  val syncVar = new SyncVar[Int]
+
+  val producer = thread {
+    var x = 0
+    while(x < 15) {
+      syncVar.putWait(x)
+      x = x + 1
+    }
+  }
+
+  val consumer = thread {
+    var x = -1
+    while(x < 14) {
+      x = syncVar.getWait
+      log(s"get: $x")
+    }
+  }
+
+  producer.join
+  consumer.join
+
+}
