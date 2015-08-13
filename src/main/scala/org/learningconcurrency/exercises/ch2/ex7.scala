84d99dd7e9869ca1a2c62b145d8160217925386e
Alexander Ryblov <ryblov.a.v@gmail.com>
2015-08-14 00:16:05 +0300

    add answer to exercise 7 chapter 2

---
+++ src/main/scala/org/learningconcurrency/exercises/ch2/ex7.scala
@@ -0,0 +1,46 @@
+package org.learningconcurrency
+package exercises
+package ch2
+
+import org.learningconcurrency.ch2._
+
+object Ex7 extends App {
+
+  import SynchronizedProtectedUid._
+
+  class Account(val name: String, var money: Int) {
+    val uid = getUniqueId()
+  }
+
+  def send(a1: Account, a2: Account, n: Int) {
+    def adjust() {
+      a1.money -= n
+      a2.money += n
+    }
+
+    a1.synchronized {
+      a2.synchronized {
+        adjust()
+      }
+    }
+
+  }
+
+  def sendAll(accounts: Set[Account], target: Account):Unit = {
+    accounts.map((a) => thread {
+      send(a,target,a.money)
+    } ).foreach(_.join)
+
+    //accounts.foreach((a) => send(a,target,a.money))
+  }
+
+  val accounts = (1 to 100).map((i) => new Account(s"Account: $i",i*10)).toSet
+  val target = new Account("Target account", 0)
+
+  sendAll(accounts,target)
+
+  accounts.foreach((a) => log(s"${a.name}, money = ${a.money}"))
+  log(s"${target.name} - money = ${target.money}")
+
+
+}
