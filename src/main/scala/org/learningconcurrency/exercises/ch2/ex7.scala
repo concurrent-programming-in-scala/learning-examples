fe256523446264667020a6b40c4d712f1e800b54
Alexander Ryblov <ryblov.a.v@gmail.com>
2015-08-19 06:38:23 +0300

    fix possibility deadlock in sendAll method

--- src/main/scala/org/learningconcurrency/exercises/ch2/ex7.scala
+++ src/main/scala/org/learningconcurrency/exercises/ch2/ex7.scala
@@ -33,11 +33,26 @@ object Ex7 extends App {
     }
   }
 
-  def sendAll(accounts: Set[Account], target: Account):Unit = {
-    accounts.map((a) => thread {
-      send(a,target,a.money)
-    } ).foreach(_.join)
 
+  def sendAll(accounts: Set[Account], target: Account): Unit = {
+
+    def adjust() = {
+      target.money = accounts.foldLeft(0)((s, a) => {
+        val money = a.money
+        a.money = 0
+        s + money
+      }
+      )
+    }
+
+    def sendAllWithSynchronize(la: List[Account]): Unit = la match {
+      case h :: t => h synchronized {
+        sendAllWithSynchronize(t)
+      }
+      case _ => adjust()
+    }
+
+    sendAllWithSynchronize((target :: accounts.toList).sortBy(_.uid))
   }
 
   val accounts = (1 to 100).map((i) => new Account(s"Account: $i",i*10)).toSet
@@ -47,6 +62,5 @@ object Ex7 extends App {
 
   accounts.foreach((a) => log(s"${a.name}, money = ${a.money}"))
   log(s"${target.name} - money = ${target.money}")
-
 
 }
