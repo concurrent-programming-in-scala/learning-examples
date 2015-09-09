8aaee6bcd00ffbfb6cdbeb78f4db00fe086fbffb
Alexander Ryblov <ryblov.a.v@gmail.com>
2015-09-09 13:17:37 +0300

    delete recover

--- src/main/scala/org/learningconcurrency/exercises/ch4/ex3.scala
+++ src/main/scala/org/learningconcurrency/exercises/ch4/ex3.scala
@@ -10,6 +10,8 @@ package ch4
  * The resulting future is completed with true if and only if the original future is completed
  * and the predicate returns true, and false otherwise.
  * You can use future combinators, but you are not allowed to create any Promise objects in the implementation.
+ *
+ * The existing Future combinators to actually fail the resulting future when the predicate p throws an exception
  */
 
 object Ex3 extends App {
@@ -18,9 +20,7 @@ object Ex3 extends App {
   import ExecutionContext.Implicits.global
 
   implicit class FutureOps[T](val self: Future[T]) {
-    def exists(p: T => Boolean): Future[Boolean] = self.map(p).recover {
-      case _ => false
-    }
+    def exists(p: T => Boolean): Future[Boolean] = self.map(p)
   }
 
   //test
@@ -31,7 +31,7 @@ object Ex3 extends App {
     -100
   }
   val f3 = Future {
-    throw new Exception("Error")
+    throw new Exception("Error in predicate")
   }
 
   def p(i: Int) = i > 0
@@ -41,5 +41,10 @@ object Ex3 extends App {
 
   log("f1 = " + Await.result(f1.exists(p), Duration.Inf).toString)
   log("f2 = " + Await.result(f2.exists(p), Duration.Inf).toString)
-  log("f3 = " + Await.result(f3.exists(p), Duration.Inf).toString)
+
+  f3.failed foreach {
+    case t:Throwable => log(t.getMessage)
+  }
+
+  //log("f3 = " + Await.result(f3.exists(p), Duration(5,"sec")).toString)
 }
