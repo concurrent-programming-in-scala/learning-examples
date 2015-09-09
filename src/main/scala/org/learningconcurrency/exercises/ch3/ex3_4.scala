f55f24c71c1e98fa346fe06557b45771fb790f8f
Alexander Ryblov <ryblov.a.v@gmail.com>
2015-09-09 13:10:46 +0300

    change implementation (add inner class Node)

--- src/main/scala/org/learningconcurrency/exercises/ch3/ex3_4.scala
+++ src/main/scala/org/learningconcurrency/exercises/ch3/ex3_4.scala
@@ -26,82 +26,83 @@ import scala.annotation.tailrec
  * and creates a constant number of new objects when there are no retries due to concurrent add invocations.
  */
 
-
 object Ex3_4 extends App {
 
   class ConcurrentSortedList[T](implicit val ord: Ordering[T]) {
 
-    private val h: AtomicReference[Option[T]] = new AtomicReference[Option[T]](None)
-    private val t: AtomicReference[Option[ConcurrentSortedList[T]]] = new AtomicReference[Option[ConcurrentSortedList[T]]](None)
+    case class Node(head: T,
+                    tail: AtomicReference[Option[Node]] = new AtomicReference[Option[Node]](None))
+
+    val root = new AtomicReference[Option[Node]](None)
 
     @tailrec
-    private def addToTail(x: T): Unit = {
-      val v = t.get
-      v match {
-        case Some(p) => p.add(x)
+    private def add(r: AtomicReference[Option[Node]], x: T): Unit = {
+      val optNode = r.get
+
+      optNode match {
         case None => {
-          val l = new ConcurrentSortedList[T]
-          l.h.set(Some(x))
-          if (!t.compareAndSet(v, Some(l))) addToTail(x)
+          if (!r.compareAndSet(optNode, Some(Node(x)))) add(r, x)
         }
+        case Some(Node(head, tail)) =>
+          if (ord.compare(x, head) > 0) {
+            //prepend new node
+            val newNode = Node(x)
+            newNode.tail.set(optNode)
+            if (!r.compareAndSet(optNode, Some(newNode))) add(r, x)
+          } else {
+            //add to tail
+            add(tail, x)
+          }
       }
     }
 
-    @tailrec
-    final def add(x: T): Unit = {
-      val optV = h.get
-
-      optV match {
-        case Some(v) => {
-          if (ord.compare(v, x) >= 0) {
-            if (h.compareAndSet(optV, Some(x))) addToTail(v)
-            else add(x)
-          } else addToTail(x)
-        }
-        case None => {
-          if (!h.compareAndSet(optV, Some(x)))
-            add(x)
-        }
-      }
+    def add(x: T): Unit = {
+      add(root, x)
     }
 
     def iterator: Iterator[T] = new Iterator[T] {
 
-      var rIter: Option[ConcurrentSortedList[T]] = Some(ConcurrentSortedList.this)
+      var rIter = root.get
 
       override def hasNext: Boolean = rIter.isEmpty == false
 
       override def next(): T = {
 
         rIter match {
-          case Some(r) => {
-            rIter = r.t.get
-            r.h.get match {
-              case Some(v) => v
-              case None => throw new NoSuchElementException("next on empty iterator")
-            }
+          case Some(node) => {
+            rIter = node.tail.get
+            node.head
           }
           case None => throw new NoSuchElementException("next on empty iterator")
         }
       }
     }
-
   }
 
   val csl = new ConcurrentSortedList[Int]()
 
 
-  (1 to 10).map((i) => thread {
+  (1 to 100).map((i) => thread {
     Thread.sleep((Math.random() * 100).toInt)
-    for (i <- 1 to 10)
+    for (i <- 1 to 1000) {
+      Thread.sleep((Math.random() * 10).toInt)
       csl.add((math.random * 100 + i).toInt)
+    }
   }
   ).foreach(_.join)
 
   log(s"length = ${csl.iterator.length}")
 
+  var prev = 0
+  var length = 0
   for (a <- csl.iterator) {
     log(a.toString)
+    if (prev > a) throw new Exception(s"$prev > $a")
+    length += 1
   }
+
+  if (csl.iterator.length != length) throw new Exception(s"${csl.iterator.length} != $length")
+
+  log(s"length = ${csl.iterator.length} ($length)")
 
 }
