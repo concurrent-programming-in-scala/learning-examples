a182918ea59880ff52d8e07c0af24676a8eb0577
Alexander Ryblov <ryblov.a.v@gmail.com>
2016-01-26 14:54:09 +0300

    change schedule method

--- src/main/scala/org/learningconcurrency/exercises/ch8/ex5.scala
+++ src/main/scala/org/learningconcurrency/exercises/ch8/ex5.scala
@@ -3,6 +3,7 @@ package exercises
 package ch8
 
 import akka.actor._
+import akka.event.Logging
 import akka.pattern._
 
 import scala.concurrent.duration._
@@ -11,75 +12,74 @@ import scala.util.{Failure, Success}
 import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
- * Implement the FailureDetector actor, which sends Identify messages to the specified
- * actors every interval seconds.
- * If an actor does not reply with any ActorIdentity messages within threshold seconds,
- * the FailureDetector actor sends a Failed message to its parent actor,
- * which contains the actor reference of the failed actor.
- */
+  * Implement the FailureDetector actor, which sends Identify messages to the specified
+  * actors every interval seconds.
+  * If an actor does not reply with any ActorIdentity messages within threshold seconds,
+  * the FailureDetector actor sends a Failed message to its parent actor,
+  * which contains the actor reference of the failed actor.
+  */
 object Ex5 extends App {
 
   class FailureDetectorActor(r: ActorRef, interval: Int, threshold: Int) extends Actor {
 
-    import org.learningconcurrency.exercises.ch8.Ex5.FailureDetectorActor._
-    import org.learningconcurrency.exercises.ch8.Ex5.ParentActor._
+    val log = Logging(context.system, this)
 
-    val cancelable = context.system.scheduler.schedule(0 seconds, interval seconds) {
-        log("send identify message")
-        (r ? Identify(r.path))(threshold seconds) onComplete {
-          case Success(_) => log("OK")
-          case Failure(e) =>
-            log(s"Not reply ")
-            log(s"parent ${context.parent}")
-            context.actorSelection(r.path.parent) ! Failed(r)
-        }
-    }
+    val cancelable = context.system.scheduler.schedule(
+      initialDelay = 0 seconds, interval = interval seconds, receiver = self, message = FailureDetectorActor.Iteration)
 
     override def receive: Receive = {
-      case StopTask =>
-        log(s"Stop task")
+      case FailureDetectorActor.Iteration =>
+        log.info("send identify message")
+        (r ? Identify(r.path))(threshold seconds) onComplete {
+          case Success(_) => log.info("OK")
+          case Failure(e) =>
+            log.info(s"$r Not reply ")
+            context.actorSelection(r.path.parent) ! ParentActor.Failed(r)
+        }
+      case FailureDetectorActor.StopTask =>
+        log.info(s"Stop task")
         cancelable.cancel()
     }
   }
 
   object FailureDetectorActor {
+    case object Iteration
     case object StopTask
     def props(r: ActorRef, interval: Int, threshold: Int) = Props(classOf[FailureDetectorActor], r, interval, threshold)
   }
 
   //test
   class TestActor extends Actor {
-    import org.learningconcurrency.exercises.ch8.Ex5.TestActor.Print
-
-    override def receive: Actor.Receive = {
-      case Print => log(s"TestActor")
-    }
+    override def receive: Actor.Receive = PartialFunction.empty
   }
 
   object TestActor {
-    case object Print
     val props = Props[TestActor]
   }
 
   class ParentActor extends Actor {
 
-    import org.learningconcurrency.exercises.ch8.Ex5.ParentActor._
-    import org.learningconcurrency.exercises.ch8.Ex5.FailureDetectorActor._
+    val log = Logging(context.system, this)
 
     val testActor = context.actorOf(TestActor.props, "TestActor")
     val failureDetectorActor = system.actorOf(FailureDetectorActor.props(testActor, 1, 2), "FailureDetector")
 
     override def receive: Actor.Receive = {
-      case StopTestActor =>
-        log("stop test actor")
+      case ParentActor.StopTestActor =>
+        log.info("stop test actor")
         context.stop(testActor)
-      case Failed(r) => failureDetectorActor ! StopTask
+      case ParentActor.Failed(r) =>
+        log.info(s"Parent. $r not reply !!!!")
+        failureDetectorActor ! FailureDetectorActor.StopTask
     }
   }
 
   object ParentActor {
-    case class Failed(r:ActorRef)
+
+    case class Failed(r: ActorRef)
+
     case object StopTestActor
+
     val props = Props[ParentActor]
 
   }
@@ -87,7 +87,7 @@ object Ex5 extends App {
   import org.learningconcurrency.exercises.ch8.Ex5.ParentActor._
 
   val system = ActorSystem("FailureDetectorSystem")
-  val parentActor = system.actorOf(ParentActor.props,"ParentActor")
+  val parentActor = system.actorOf(ParentActor.props, "ParentActor")
 
   Thread.sleep(5000)
   parentActor ! StopTestActor
